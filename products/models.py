3cd8b07cde0d18df24479b70fe9114e07a686672
YuriyNik156 <yuriy.nik156@gmail.com>
2025-09-03 15:33:01 +0300

    add class Meta and ForeignKey

--- products/models.py
+++ products/models.py
@@ -5,6 +5,10 @@ class Shop(models.Model):
     name = models.CharField(max_length=255)
     address = models.TextField()
 
+    class Meta:
+        verbose_name = "Магазин"
+        verbose_name_plural = "Магазины"
+
     def __str__(self):
         return f"{self.name} - {self.address}"
 
@@ -13,10 +17,14 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
-    shop_addresses = models.ManyToManyField("Shop", related_name="products")
+    shop = models.ForeignKey("Shop", on_delete=models.CASCADE, related_name="products")
+
+    class Meta:
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
 
     def __str__(self):
-        return self.name
+        return f"{self.name} ({self.shop.name})"
 
 class CustomUser(AbstractUser):
     ROLE_CHOICES = [
@@ -24,7 +32,12 @@ class CustomUser(AbstractUser):
         ('sales_executive', 'Sales Executive'),
         ('user', 'User')
     ]
+
+    email = models.EmailField(unique=True)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return f"{self.username} ({self.role})"
