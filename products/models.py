847aa02efe19b562d5213c7cb6190f55fe6459cf
YuriyNik156 <yuriy.nik156@gmail.com>
2025-09-03 15:11:55 +0300

    add Product, Shop and CustomUser in models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class Shop(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+
+    def __str__(self):
+        return f"{self.name} - {self.address}"
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    shop_addresses = models.ManyToManyField("Shop", related_name="products")
+
+    def __str__(self):
+        return self.name
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('sales_executive', 'Sales Executive'),
+        ('user', 'User')
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+
