737ce28fe2097a01ae5bc0dc820d981a81ccfb31
YuriyNik156 <yuriy.nik156@gmail.com>
2025-09-05 12:45:23 +0300

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
