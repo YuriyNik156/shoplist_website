d2b89e0cc027bf742f0bf8b9c4532909aacc8cf1
YuriyNik156 <yuriy.nik156@gmail.com>
2025-09-05 18:22:15 +0300

    shoplist\urls.py corrected

--- shoplist/urls.py
+++ shoplist/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", lambda request: redirect("products")),  # редирект с главной на товары
-    path("", include("products.urls")),  # подключаем урлы из приложения
+    path("", lambda request: redirect("products")),
+    path("", include("products.urls")),
 ]
