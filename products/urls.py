50afe2e2df16606c352a4adac424fbd97b9360d0
YuriyNik156 <yuriy.nik156@gmail.com>
2025-09-06 12:54:14 +0300

    add edit and delete products in products\urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,15 +1,17 @@
 from django.urls import path
-from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import (RegisterView, ProductListView, ProductCreateView, ProductUpdateView, ProductDeleteView,)
 
 urlpatterns = [
     # Аутентификация
-    path("register/", views.RegisterView.as_view(), name="register"),
+    path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
-    path("logout/", LogoutView.as_view(next_page=reverse_lazy("login")), name="logout"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
 
     # Товары
-    path("products/", views.ProductListView.as_view(), name="products"),
-    path("products/add/", views.ProductCreateView.as_view(), name="product_add"),
+    path("products/", ProductListView.as_view(), name="products"),
+    path("products/add/", ProductCreateView.as_view(), name="product_add"),
+    path("products/<int:pk>/edit/", ProductUpdateView.as_view(), name="product_edit"),
+    path("products/<int:pk>/delete/", ProductDeleteView.as_view(), name="product_delete"),
 ]
