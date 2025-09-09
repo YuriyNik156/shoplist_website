d3b23807053e879c027a1c9b63978dfa81fa32ab
YuriyNik156 <yuriy.nik156@gmail.com>
2025-09-09 16:38:10 +0300

    update models.py

--- products/models.py
+++ products/models.py
@@ -19,7 +19,9 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
-    shop_address = models.CharField(max_length=255, verbose_name="Адрес магазина")
+
+    shop_name = models.CharField(max_length=255)
+    shop_address = models.CharField(max_length=255)
 
     class Meta:
         verbose_name = "Товар"
