ee1fc1537d32c068ba36fd74fe04652e62f93ce8
YuriyNik156 <yuriy.nik156@gmail.com>
2025-09-11 18:57:06 +0300

    update models.py

--- products/models.py
+++ products/models.py
@@ -20,7 +20,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
     shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="products")
-    address = models.CharField(max_length=255)
 
     class Meta:
         verbose_name = "Товар"
