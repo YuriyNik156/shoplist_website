409d2f02bcfbfdfeb22565fdad170d2c560ef48e
YuriyNik156 <yuriy.nik156@gmail.com>
2025-09-11 15:27:35 +0300

    add ability to view shops in views.py

--- products/views.py
+++ products/views.py
@@ -1,13 +1,14 @@
 from django.contrib.auth import login
 from django.db.models import Q
 from django.urls import reverse_lazy
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView, DetailView
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.views import LoginView
+from django.core.paginator import Paginator
 
 from .forms import ProductForm, CustomUserCreationForm
-from .models import CustomUser, Product
+from .models import CustomUser, Product, Shop
 
 # Регистрация
 class RegisterView(CreateView):
@@ -33,18 +34,28 @@ class ProductListView(LoginRequiredMixin, ListView):
     model = Product
     template_name = "products/product_list.html"
     context_object_name = "products"
-    paginate_by = 10
+    paginate_by = 6
     login_url = '/login/'
     redirect_field_name = 'next'
 
     def get_queryset(self):
         queryset = super().get_queryset()
         query = self.request.GET.get("q")
+        shop_id = self.request.GET.get("shop")
         if query:
             queryset = queryset.filter(
                 Q(name__icontains=query) |  Q(description__icontains=query)
             )
+        if shop_id:
+            queryset = queryset.filter(shop_id=shop_id)
         return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["stores"] = Shop.objects.all()
+        context["q"] = self.request.GET.get("q", "")
+        context["selected_store"] = self.request.GET.get("shop", "")
+        return context
 
 # Детальное представление товара (для авторизованных пользователей)
 class ProductDetailView(LoginRequiredMixin, DetailView):
