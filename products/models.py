ff819bd67565a3f8126f34c0cb668245c071b747
YuriyNik156 <yuriy.nik156@gmail.com>
2025-09-11 16:06:38 +0300

    update product_list.html

--- products/models.py
+++ products/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # БД магазинов
 class Shop(models.Model):
-    name = models.CharField(max_length=255)
-    address = models.TextField()
+    name = models.CharField(max_length=100)
+    address = models.TextField(max_length=255)
 
     class Meta:
         verbose_name = "Магазин"
@@ -19,7 +19,7 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='products/', blank=True, null=True)
-    shop_address = models.CharField(max_length=255)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE, related_name="products")
 
     class Meta:
         verbose_name = "Товар"
