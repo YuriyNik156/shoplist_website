ecdd35e00b5b055f294b4002e0a995c22c603cd9
YuriyNik156 <yuriy.nik156@gmail.com>
2025-09-16 11:16:15 +0300

    Update models.py

--- products/models.py
+++ products/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# БД магазинов
+
 class Shop(models.Model):
+    """Модель магазина."""
+
     name = models.CharField(max_length=100)
     address = models.TextField(max_length=255)
 
@@ -13,8 +15,10 @@ class Shop(models.Model):
     def __str__(self):
         return f"{self.name} - {self.address}"
 
-# БД товаров
+
 class Product(models.Model):
+    """Модель товара."""
+
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -28,11 +32,13 @@ class Product(models.Model):
     def __str__(self):
         return f"{self.name} ({self.shop})"
 
-# Кастомная модель пользователя
+
 class CustomUser(AbstractUser):
+    """Кастомная модель пользователя с email и ролью."""
+
     ROLE_CHOICES = [
         ('sales_executive', 'Sales Executive'),
-        ('user', 'User')
+        ('user', 'User'),
     ]
 
     email = models.EmailField(unique=True)
